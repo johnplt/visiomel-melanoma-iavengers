cc32d9060c79c06d53826fc77919cd38f0c5a10d
johnplt <johnplt@live.com>
2023-04-21 12:00:56 +0200

    update

--- lib_system_requirement.sh
+++ lib_system_requirement.sh
@@ -1,3 +1,5 @@
 sudo apt-get update
 sudo apt-get install libgl1
 sudo apt-get install libvips
+sudo apt-get install awscli
+aws s3 cp s3://drivendata-competition-visiomel-public-us/images/ images/ --no-sign-request --recursive
