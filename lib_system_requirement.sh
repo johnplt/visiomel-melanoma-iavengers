7d0ac905dc13fc83aa192b69bcb2a035eb5d30f0
johnplt <johnplt@live.com>
2023-04-26 16:14:49 +0200

    update

--- lib_system_requirement.sh
+++ lib_system_requirement.sh
@@ -3,4 +3,5 @@ sudo apt-get install libgl1
 sudo apt-get install libvips
 sudo apt-get install awscli
 sudo apt-get install zip
-mkdir images
+mc cp -r s3/jplaton/diffusion/visio_mel/images/images/ data/images
+mc cp -r s3/etruong/diffusion/visio_mel/images/ data/images
